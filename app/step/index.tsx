0319bacebdc7e1b750092aeec4f30874e4c9db79
Lucas Estefano <lucccasestefano1@gmail.com>
2025-05-28 23:19:41 +1000

    Fixed header

---
+++ app/step/index.tsx
@@ -0,0 +1,11 @@
+import { Image, Pressable, StyleSheet, Text, View } from 'react-native';
+import { colors } from '../../app-example/constants/colors';
+import { Header} from '../../app-example/components/header/index'
+
+export default function Step(){
+    return(
+        <View>
+            <Header/>
+        </View>
+    )
+}
