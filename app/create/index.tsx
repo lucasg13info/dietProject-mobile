99e56716a28d25a2e195bebe5450ddbad8606107
Lucas Estefano <lucccasestefano1@gmail.com>
2025-05-30 10:39:02 +1000

    New form options

--- app/create/index.tsx
+++ app/create/index.tsx
@@ -30,6 +30,22 @@ export default function Create() {
         {label: "Feminino", value: "Feminino"}
   ]
 
+  const levelOptions = [
+    { label: 'Sedentário (pouco ou nenhuma atividade física)', value: 'Sedentário' },
+    { label: 'Levemente ativo (exercícios 1 a 3 vezes na semana)', value: 'Levemente ativo (exercícios 1 a 3 vezes na semana)' },
+    { label: 'Moderadamente ativo (exercícios 3 a 5 vezes na semana)', value: 'Moderadamente ativo (exercícios 3 a 5 vezes na semana)' },
+    { label: 'Altamente ativo (exercícios 5 a 7 dia por semana)', value: 'Altamente ativo (exercícios 5 a 7 dia por semana)' },
+  ]
+
+  const objectiveOptions = [
+    { label: 'Emagrecer', value: 'emagrecer' },
+    { label: 'Hipertrofia', value: 'Hipertrofia' },
+    { label: 'Hipertrofia + Definição', value: 'Hipertrofia e Definição' },
+    { label: 'Definição', value: 'Definição' },
+  ]
+
+
+
  return (
    <View style={styles.container}>
     
@@ -39,7 +55,7 @@ export default function Create() {
     />
 
     <ScrollView style={styles.content}>
-      <Text style={styles.label}>Sexo</Text>
+      <Text style={styles.label}>Sexo:</Text>
       <Select
         control={control}
         name= "gender"
@@ -47,6 +63,27 @@ export default function Create() {
         error={errors.gender?.message}
         options={genderOptions}
       />
+
+      <Text style={styles.label}>Selecione seu nivel de atividade fisica:</Text>
+      <Select
+        control={control}
+        name= "level"
+        placeholder="Selecione o seu sexo"
+        error={errors.level?.message}
+        options={levelOptions}
+      />
+      <Text style={styles.label}>Selecione seu objetivo:</Text>
+      <Select
+        control={control}
+        name= "objective"
+        placeholder="Selecione o seu sexo"
+        error={errors.objective?.message}
+        options={objectiveOptions}
+      />
+
+      
+
+      
 
     </ScrollView>
 
